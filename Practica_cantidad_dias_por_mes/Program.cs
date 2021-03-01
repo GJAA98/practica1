af8f7a90e06619bc6ea4b2bb34ff2c7fb7b63aac
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:34:57 -0400

    Add files via upload
    Un programa que almacene en un array el número de días que tiene cada mes (supondremos que es un año no bisiesto), pida al usuario que le indique un mes (1=enero, 12=diciembre) y muestre en pantalla el número de días que tiene ese mes.

---
+++ Practica_cantidad_dias_por_mes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Practica_cantidad_dias_por_mes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = new string[]
+            {
+                "Enero",
+                "Febrero",
+                "Marzo",
+                "Abril",
+                "Mayo",
+                "Junio",
+                "Julio",
+                "Agosto",
+                "Septiembre",
+                "Octubre",
+                "Noviembre",
+                "Diciembre"
+            };
+
+            int[] cantidadDeDias = new int[]
+            {
+                31,28,31,30,31,30,31,31,30,31,30,31
+            };
+
+            
+            Console.WriteLine("Meses del Año:");
+            for (int i = 0; i < 12; i++)
+            {
+                int mes = i + 1;
+                Console.WriteLine(mes+" - " +meses[i]);
+            }
+            Console.WriteLine("Seleccione un mes:");
+
+            string valor = Console.ReadLine();
+            int mesSeleccionado = int.Parse(valor);
+
+            for (int i = 1; i <= 12; i++)
+            {
+                if (i == mesSeleccionado)
+                {                    
+                    Console.WriteLine("Cantidad de dias:"+ cantidadDeDias[i-1]);
+                    break;
+                }
+            }
+        }
+    }
+}
